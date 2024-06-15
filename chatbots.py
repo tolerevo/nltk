44809a50a560f401178a0c956f0fe452f26860cc
Diego Tolentino <tolerevo@hotmail.com>
2024-06-15 00:44:24 -0600

    mend

--- chatbots.py
+++ chatbots.py
@@ -1,6 +1,3 @@
-import nltk
-nltk.download()
-
 from nltk.tokenize import word_tokenize, sent_tokenize
 
 example_text = "Hello Mr Smith, how are you doing today? The weather is great and Python is awesome. The sky is pinkish-blue. You shouldn't eat cardboard."
